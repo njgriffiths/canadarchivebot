2a2adf7306f8e8f92181dbec006ddaff80b08f1a
mr_ <njgriffiths@gmail.com>
2018-06-21 13:35:16 -0700

    lookup table for bot reply text

---
+++ js/twitter-replies.js
@@ -0,0 +1,10 @@
+module.exports = {
+	follow: {
+		reply_01: 'Thanks for the follow @',
+		reply_02: 'I’m a pretty simple bot so take a look at the hashtags in some of my previous posts to see what kind of requests I can understand.'
+	},
+	no_tags: [
+		'I don’t understand. Try one of these: arts, culture, environment, exploration, immigration, industry, military, sports or the name of a province (without spaces)',
+		'Hmmm... How about something more specific: AeriaPohotos, tintypes, sugarshacks, portraits, PrimeMinisters, NationalParks, artic, FirstNations, HalifaxExplosion, Expo67, etc.'
+	]
+}
